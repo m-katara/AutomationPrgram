0e47402b7d011d3b51adbef088e9d4eb948eb382
m-katara <mayur.katara10@gmail.com>
2024-10-18 09:18:00 +0530

    Update Assignment30.java

--- Assignment30.java
+++ Assignment30.java
@@ -1,5 +1,17 @@
 package Array;
-
-public class Assignment30 {
+import java.util.Arrays;
+public class Assignment30 
+{
+	public static void main(String[] args) 
+	{
+		int num[]= new int[3];
+		num[0]=10;
+		num[1]=20;
+		num[2]=30;
+		System.out.println(Arrays.toString(num));
+		num[2]=40;									//updating array value
+		System.out.println(Arrays.toString(num));
 
 }
+}
+
